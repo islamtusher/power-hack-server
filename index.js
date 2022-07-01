337d52363a2e6251a7d0dcdf0764db6428e10c32
Rafiqul Islam Tusher <md.rafiqul26997@gmail.com>
2022-07-01 15:44:32 +0600

    change the db user and pass by .env variable

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xlljd.mongodb.net/?retryWrites=true&w=majority`;
 // const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const uri = "mongodb+srv://power-hack:tCCTbVgKXjsIs1Hd@cluster0.hv5m8.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hv5m8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
